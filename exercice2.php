e4169a5bb29bfda0a7f1c0a0f2b6677f61ada4ba
1Seydou-12-diarra <seydou.diarrassouba@imako.digital>
2023-02-20 18:23:12 +0100

    update

--- exercice2.php
+++ exercice2.php
@@ -1,25 +1,30 @@
 <?php 
 // 1- Créez une connexion de base de données au serveur PostgreSQL.
-$host = 'your_host_name';
-$dbname = 'your_database_name';
-$user = 'your_username';
-$password = 'your_password';
 
-try {
-    $conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully\n";
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+$host = 'localhost';
+$dbname = 'dvd';
+$username = 'postgres';
+$password = '59898060';
+
+$dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password";
+
+try{
+ $conn = new PDO($dsn);
+ 
+ if($conn){
+  echo "Connecté à $dbname avec succès!";
+ }
+}catch (PDOException $e){
+ echo $e->getMessage();
 }
 
 
-// 2- Créez une requête SQL.
+// // 2- Créez une requête SQL.
 $sql = "DELETE FROM COMPANY WHERE ID = 2";
 
 
 // 3- Exécuter une requête pour supprimer des lignes de la table
-$conn->exec($sql);
+
 
 //  Récupérez et affichez les enregistrements restants de la COMPANYtable à l'aide d'une instruction SELECT 
 $sql = "SELECT * FROM COMPANY";
@@ -27,7 +32,9 @@ $stmt = $conn->query($sql);
 while ($row = $stmt->fetch()) {
     echo "ID: " . $row['ID'] . ", Name: " . $row['NAME'] . ", Age: " . $row['AGE'] . ", Salary: " . $row['SALARY'] . "\n";
 }
- // 4- Fermez la connexion à la base de données
+// //  // 4- Fermez la connexion à la base de données
  $conn = null;
+
+  
 
 ?>
